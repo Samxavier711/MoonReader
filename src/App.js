b4da0effc138f4ceac55eade703f0809f1c334ce
Aditya Mishra <adityamishra4727@gmail.com>
2023-04-16 12:32:31 +0530

    'file'

--- src/App.js
+++ src/App.js
@@ -2,23 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+    <div class="nav_bar">
+    <div class="all_navigation">
+
+    <img class="logo" src="RiseandShine.png" border="none" width="60" height="auto" />
+
+    <ul class="nav_bar_links_ul">
+
+      <li class="dropdown menulinks">
+        Moonreader
+        
+      </li>
+
+      
+
+      <li class="nav_bar_links_ul" >
+        <a  href="#">                 Contribute</a>
+        
+      </li>
+
+      
+      
+    </ul>
+  </div>
+  
+</div>
+<div class="flex">
+    <button >
+      Click
+    </button>
+    <button>
+      Upload
+    </button>
+
+
+  </div>
+    
+    </>
   );
 }
 
